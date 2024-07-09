62129316411925b617b003175f11af03fec1cd48
Jisung15 <gimseongjin302@gmail.com>
2024-07-09 16:30:33 +0900

    mbti 검사 앱 사전캠프때 한 코드 커밋

---
+++ app/src/main/java/com/example/mbtitest/TestActivity.kt
@@ -0,0 +1,52 @@
+package com.example.mbtitest
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.viewpager2.widget.ViewPager2
+
+class TestActivity : AppCompatActivity() {
+
+    private lateinit var viewPager: ViewPager2
+
+    val questionNairResults = QuestionNairResults()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_test)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        viewPager = findViewById(R.id.viewPager)
+        viewPager.adapter = ViewPagerAdapter(this)
+        viewPager.isUserInputEnabled = false
+    }
+
+    fun moveToNextQuestion() {
+        if (viewPager.currentItem == 3) {
+
+            // 마지막 페이지 -> 결과 화면으로 이동
+
+        } else {
+            val nextItem = viewPager.currentItem + 1
+            if (nextItem < viewPager.adapter?.itemCount ?: 0) {
+                viewPager.setCurrentItem(nextItem, true)
+            }
+        }
+    }
+}
+
+class QuestionNairResults {
+    val results = mutableListOf<Int>()
+
+    fun addResponses(response:List<Int>) {
+        val mostFrequent = response.groupingBy { it }.eachCount().maxByOrNull { it.value }?.key
+        mostFrequent?.let { results.add(it) }
+    }
+}
